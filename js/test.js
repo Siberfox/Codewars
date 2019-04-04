a9e4db335731246a5f9eb880f1346f36a90855fb
Andrew <greyfoxjob@gmail.com>
2019-04-04 15:55:08 +0600

    te

---
+++ js/test.js
@@ -0,0 +1,23 @@
+let a = 3;
+let b = 5;
+function med(c){
+   return a + b - c;  
+};
+
+console.log(Math.sqrt(med(4)));
+console.log('hi');
+console.log(b);
+
+let array = [1,2,3,4];
+console.log(array.length);
+
+array.push (9,10);
+console.log(array.length);
+array
+
+let f = 'me';
+array.push (f);
+array
+
+let array2 = array.map (number => number * number) //Function (es6)
+array2
