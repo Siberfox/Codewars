e6b3b9d418448e9fc439b59f09e105763529b562
Siberfox <greyfoxjob@gmail.com>
2020-03-03 19:50:07 +0600

    eslint/jest plugin config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     commonjs: true
   },
-  extends: ['airbnb', 'prettier'],
+  extends: ['airbnb', 'prettier', 'plugin:jest/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -19,13 +19,18 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'jest'],
   rules: {
     'no-plusplus': 'off',
     'no-alert': 'off',
     'no-unused-vars': 'warn',
     'no-console': 'off',
     'prettier/prettier': ['error'],
-    'prefer-const': 'warn'
+    'prefer-const': 'warn',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error'
   }
 };
