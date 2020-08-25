8adeb7c94d85c5dff29724f7da8e6205e1946880
Andrew I <greyfoxjob@gmail.com>
2020-08-25 20:47:09 +0600

    longest common subsequence

--- codewars.js
+++ codewars.js
@@ -1,66 +1,26 @@
-// Sudoku Solution Validator
+//
+// Longest Common Subsequence
 
-function validSolution(arr) {
-  let firstSqr = [];
-  let secondSqr = [];
-  let thirdSqr = [];
+function lcs(a, b) {
+  const obj = a
+    .toLowerCase()
+    .split('')
+    .reduce((acc, val) => {
+      acc[val] ? (acc[val] += 1) : (acc[val] = 1);
+      return acc;
+    }, {});
 
-  for (let i = 0; i < arr.length; i++) {
-    const row = [];
-    const col = [];
-
-    if (i === 3 || i === 6) {
-      firstSqr = [];
-      secondSqr = [];
-      thirdSqr = [];
-    }
-
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j][i] === 0 || arr[i][j] === 0) {
-        return false;
-      }
-
-      if (row[arr[i][j]] || col[arr[j][i]]) {
-        return false;
-      }
-      row[arr[i][j]] = arr[i][j];
-      col[arr[j][i]] = arr[j][i];
-
-      if (j < 3) {
-        if (firstSqr[arr[i][j]]) {
-          return false;
-        }
-        firstSqr[arr[i][j]] = arr[i][j];
-      }
-      if (j >= 3 && j < 6) {
-        if (secondSqr[arr[i][j]]) {
-          return false;
-        }
-        secondSqr[arr[i][j]] = arr[i][j];
-      }
-      if (j >= 6) {
-        if (thirdSqr[arr[i][j]]) {
-          return false;
-        }
-        thirdSqr[arr[i][j]] = arr[i][j];
-      }
+  const result = [];
+  for (let i = 0; i < b.length; i++) {
+    if (obj[b[i]]) {
+      result.push(b[i]);
+      obj[b[i]] -= 1;
     }
   }
-  return true;
+  return result.join('');
 }
 
-module.exports = validSolution;
-
-console.log(
-  validSolution([
-    [5, 3, 4, 6, 7, 8, 9, 1, 2],
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-  ])
-);
+console.log(lcs('abcDefef', 'abc')); // "abc"
+console.log(lcs('abcdef', 'acfz')); // "acf"
+console.log(lcs('132535365', '123456789')); // "12356"
+console.log(lcs('anothertest', 'notatest')); // "12356"
--- completed/completed.js
+++ completed/completed.js
@@ -37,10 +37,7 @@
 //   if (i % 5 === 0 && i > 0) {
 //     result += 'Buzz';
 //   }
-//   if (result === '') {
-//     result = i;
-//   }
-//   console.log(result);
+//   console.log(result || i);
 // }
 
 // 4 ------------------------------------------------------------------------
@@ -901,4 +898,73 @@
 // console.log(whoIsNext(names, 52));
 // console.log(whoIsNext(names, 1));
 // console.log(whoIsNext(names, 7230702951));
+
+// ---------------------------------------------------------------------
+
+// Sudoku Solution Validator
+
+// function validSolution(arr) {
+//   let firstSqr = [];
+//   let secondSqr = [];
+//   let thirdSqr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const row = [];
+//     const col = [];
+
+//     if (i === 3 || i === 6) {
+//       firstSqr = [];
+//       secondSqr = [];
+//       thirdSqr = [];
+//     }
+
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j][i] === 0 || arr[i][j] === 0) {
+//         return false;
+//       }
+
+//       if (row[arr[i][j]] || col[arr[j][i]]) {
+//         return false;
+//       }
+//       row[arr[i][j]] = arr[i][j];
+//       col[arr[j][i]] = arr[j][i];
+
+//       if (j < 3) {
+//         if (firstSqr[arr[i][j]]) {
+//           return false;
+//         }
+//         firstSqr[arr[i][j]] = arr[i][j];
+//       }
+//       if (j >= 3 && j < 6) {
+//         if (secondSqr[arr[i][j]]) {
+//           return false;
+//         }
+//         secondSqr[arr[i][j]] = arr[i][j];
+//       }
+//       if (j >= 6) {
+//         if (thirdSqr[arr[i][j]]) {
+//           return false;
+//         }
+//         thirdSqr[arr[i][j]] = arr[i][j];
+//       }
+//     }
+//   }
+//   return true;
+// }
+
+// module.exports = validSolution;
+
+// console.log(
+//   validSolution([
+//     [5, 3, 4, 6, 7, 8, 9, 1, 2],
+//     [6, 7, 2, 1, 9, 5, 3, 4, 8],
+//     [1, 9, 8, 3, 4, 2, 5, 6, 7],
+//     [8, 5, 9, 7, 6, 1, 4, 2, 3],
+//     [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//     [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//     [9, 6, 1, 5, 3, 7, 2, 8, 4],
+//     [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//     [3, 4, 5, 2, 8, 6, 1, 7, 9]
+//   ])
+// );
 
